e6acbf40c5b9f5363fbad8d1c6330e3afd256890
melaniemdm <melanieborja.pro@gmail.com>
2025-02-06 11:47:16 +0100

    feature : add method to extract user id

--- back/src/main/java/com/openclassrooms/mddapi/controller/CommentaireController.java
+++ back/src/main/java/com/openclassrooms/mddapi/controller/CommentaireController.java
@@ -3,6 +3,7 @@ package com.openclassrooms.mddapi.controller;
 import com.openclassrooms.mddapi.dto.CommentaireDTO;
 
 import com.openclassrooms.mddapi.service.CommentaireService;
+import com.openclassrooms.mddapi.service.JwtService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,7 +17,8 @@ import java.util.Map;
 public class CommentaireController {
 @Autowired
  private CommentaireService commentaireService;
-
+    @Autowired
+    private JwtService jwtService;
 //constructeur
     public CommentaireController(CommentaireService commentaireService){
         this.commentaireService = commentaireService;
@@ -31,9 +33,41 @@ public class CommentaireController {
     }
 
     @PostMapping
-     public ResponseEntity<Map<String, CommentaireDTO>> createCommentaire(@RequestBody CommentaireDTO commentaireDTO) {
-        CommentaireDTO createdDTO = commentaireService.createCommentaire(commentaireDTO);
+     public ResponseEntity<Map<String, CommentaireDTO>> createCommentaire(@RequestBody CommentaireDTO commentaireDTO, @RequestHeader("Authorization") String authHeader) {
+        try{
+            // 🔥Extraire l'ID utilisateur depuis le token
+            Long userId = extractUserIdFromToken(authHeader);
 
-        return new ResponseEntity<>(Map.of("comment", createdDTO), HttpStatus.CREATED);
+            //  Convertir Long en Integer
+            Integer userIdAsInteger = Math.toIntExact(userId);
+
+            // Assigner l'ID utilisateur au DTO
+            commentaireDTO.setAuteur_id(userIdAsInteger);
+
+            CommentaireDTO createdDTO = commentaireService.createCommentaire(commentaireDTO);
+            return new ResponseEntity<>(Map.of("comment", createdDTO), HttpStatus.CREATED);
+        }catch (IllegalArgumentException exception) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body(Map.of("error", new CommentaireDTO()));
+
+        }
+    }
+
+    private Long extractUserIdFromToken(String authHeader) {
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            throw new IllegalArgumentException("Missing or invalid token");
+        }
+
+        // Extraire le token (sans "Bearer ")
+        String token = authHeader.substring(7);
+
+        // Extraire l'ID utilisateur depuis le token
+        Long userId = jwtService.extractUserId(token);
+
+        if (userId == null) {
+            throw new IllegalArgumentException("Invalid token");
+        }
+
+        return userId;
     }
 }
