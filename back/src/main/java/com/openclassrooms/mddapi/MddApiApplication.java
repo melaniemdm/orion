de126c2fbbdda2af9a6afe5a885eb9369daa2974
melaniemdm <melanieborja.pro@gmail.com>
2025-02-17 13:57:31 +0100

    fix: delete exclude security configuration for encryption

--- back/src/main/java/com/openclassrooms/mddapi/MddApiApplication.java
+++ back/src/main/java/com/openclassrooms/mddapi/MddApiApplication.java
@@ -3,8 +3,7 @@ package com.openclassrooms.mddapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-//exclude security config pour encryption
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+
 public class MddApiApplication {
 
 	public static void main(String[] args) {
