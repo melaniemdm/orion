caa7594f1aafb09a65a53374695c906b5a070f68
melaniemdm <melanieborja.pro@gmail.com>
2025-02-18 13:09:33 +0100

    feature : create header composant

---
+++ front/src/app/components/header/header.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+  
+  // Contrôle l’affichage du menu en mobile
+  public isMenuOpen: boolean = false;
+
+  constructor() { }
+
+  ngOnInit(): void { }
+
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+}
