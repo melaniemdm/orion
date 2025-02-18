5473ba5772868d7e791169f57446f62ac0fbbd5e
melaniemdm <melanieborja.pro@gmail.com>
2025-02-18 16:26:36 +0100

    fix :  add path subscription and login

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -5,14 +5,20 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ButtonComponent } from './components/button/button.component';
+import { FormConnexionComponent } from './components/form-connexion/form-connexion.component';
+import { SubscriptionComponent } from './pages/subscription/subscription.component';
+import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [AppComponent, HomeComponent, HeaderComponent, ButtonComponent, SubscriptionComponent, LoginComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatButtonModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent],
