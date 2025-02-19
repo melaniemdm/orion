11d2660944c2fbb969a33c0ba96b71ad298f47c6
melaniemdm <melanieborja.pro@gmail.com>
2025-02-19 14:28:07 +0100

    feature : create subcription component

---
+++ front/src/app/pages/subscription/subscription.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-subscription',
+  templateUrl: './subscription.component.html',
+  styleUrls: ['./subscription.component.scss'],encapsulation: ViewEncapsulation.None
+})
+export class SubscriptionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    
+  }
+
+}
