13d0bfc8a868d7d189d3c18beef63923a3d5abdd
melaniemdm <melanieborja.pro@gmail.com>
2025-03-10 13:52:26 +0100

    refacto :  remove button

--- front/src/app/components/button/button.component.ts
+++ front/src/app/components/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -10,10 +10,9 @@ export class ButtonComponent implements OnInit {
   @Input() action: string = '';
   @Input() routerLink?: string; 
 
-  onClick(): void {
-    console.log(`${this.action} button clicked`);
-  }
- 
+  
+
+  
 
   constructor() { }
 
