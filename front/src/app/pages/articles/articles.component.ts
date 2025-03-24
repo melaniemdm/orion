21f139980ece738b6ab3bbeb0a401e6fe60aa313
melaniemdm <melanieborja.pro@gmail.com>
2025-03-24 17:23:18 +0100

    feature : front - item sorting function with arrow

--- front/src/app/pages/articles/articles.component.ts
+++ front/src/app/pages/articles/articles.component.ts
@@ -16,30 +16,26 @@ export class ArticlesComponent implements OnInit {
 // On stocke la liste des articles ici
 public articles: ArticleRequest[] = [];
 public users: User[] = [];
+sortDescending: boolean = true;
 
 constructor(private articleService: ArticleService, private userService: UserService, private router: Router) {}
 
 ngOnInit(): void {
   const token = localStorage.getItem('token');
 
-    if (!token) {
-      // S'il n'y a pas de token, redirige vers la home directement
-      this.router.navigate(['/home']);
-      return; // Important de s'arrêter ici
-    }
-    
+  if (!token) {
+    this.router.navigate(['/home']);
+    return;
+  }
+
   forkJoin([
     this.articleService.getAllArticles(),
     this.userService.getAllUsers()
   ]).subscribe({
     next: ([articles, users]) => {
-      this.articles = articles.sort((a, b) => {
-        const dateA = new Date(a.created_date).getTime();
-        const dateB = new Date(b.created_date).getTime();
-        return dateB - dateA; 
-      });
-
+      this.articles = articles;
       this.users = users;
+      this.sortArticles(); // Effectue le tri initial dès la récupération
     },
     error: (err) => {
       console.error('Erreur lors de la récupération :', err);
@@ -48,5 +44,17 @@ ngOnInit(): void {
 }
 
 
+toggleSortOrder(): void {
+  this.sortDescending = !this.sortDescending;
+  this.sortArticles();
+}
+sortArticles(): void {
+  this.articles.sort((a, b) => {
+    const dateA = new Date(a.created_date).getTime();
+    const dateB = new Date(b.created_date).getTime();
+
+    return this.sortDescending ? dateB - dateA : dateA - dateB;
+  });
+}
 
 }
