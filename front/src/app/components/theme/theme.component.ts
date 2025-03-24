db5b50da2b2c09d1ffb079b59af89f6016362854
melaniemdm <melanieborja.pro@gmail.com>
2025-03-24 12:37:53 +0100

    fix: front - add subscribe button functionality

--- front/src/app/components/theme/theme.component.ts
+++ front/src/app/components/theme/theme.component.ts
@@ -18,7 +18,37 @@ export class ThemeComponent implements OnInit {
   constructor(private subscriptionService: SubscriptionService, private authService: AuthService, private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.subscriptionService.getUserSubscriptions().subscribe(subscriptions => {
+      this.subscribed = subscriptions.some(sub => sub.theme_id === this.theme.id);
+      console.log('État initial abonnement :', this.subscribed);
+    });
   }
-  toggleSubscription() {
+  
+ 
+
+  // Vérifie si l’utilisateur est déjà abonné à ce thème
+  toggleSubscription(): void {
+    console.log('État abonnement avant action :', this.subscribed);
+
+    if (!this.subscribed) {
+      console.log('Tentative abonnement au thème ID :', this.theme.id);
+      this.subscriptionService.subscribeToTheme(this.theme.id)
+        .subscribe(() => {
+          this.subscribed = true;
+          console.log('Abonnement réussi au thème ID :', this.theme.id);
+        }, err => {
+          console.error('Erreur abonnement (subscribeToTheme) :', err);
+        });
+    } else {
+      console.log('Tentative désabonnement du thème ID :', this.theme.id);
+      this.subscriptionService.unsubscribeFromTheme(this.theme.id)
+        .subscribe(() => {
+          this.subscribed = false;
+          console.log('Désabonnement réussi du thème ID :', this.theme.id);
+        }, err => {
+          console.error('Erreur désabonnement (unsubscribeFromTheme) :', err);
+        });
+    }
   }
 }
+
--- front/src/app/services/subscribe.service.ts
+++ front/src/app/services/subscribe.service.ts
@@ -1,16 +1,61 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
+import { catchError, map, Observable, of, tap, throwError } from "rxjs";
 
 @Injectable({ providedIn: 'root' })
 export class SubscriptionService {
   constructor(private http: HttpClient) { }
 
+  private getAuthHeaders(): HttpHeaders {
+    const token = localStorage.getItem('token');
+    console.log('Token récupéré :', token);
+    return new HttpHeaders({
+      'Authorization': `Bearer ${token}`,
+      'Content-Type': 'application/json'
+    });
+  }
+
   subscribeToTheme(themeId: number): Observable<any> {
-    return this.http.post('/api/subscription', { themeId });
+    console.log('Requête abonnement envoyée au serveur, themeId :', themeId);
+    return this.http.post('/api/subscription', { theme_id: themeId }, {
+      headers: this.getAuthHeaders()
+    })
+    .pipe(
+      tap(res => console.log('Réponse abonnement serveur :', res)),
+      catchError(err => {
+        console.error('Erreur HTTP abonnement serveur :', err);
+        return throwError(() => err);
+      })
+    );
   }
 
   unsubscribeFromTheme(themeId: number): Observable<any> {
-    return this.http.delete(`/api/subscription/${themeId}`);
+    console.log('Requête désabonnement envoyée au serveur, themeId :', themeId);
+    return this.http.delete(`/api/subscription/${themeId}`, {
+      headers: this.getAuthHeaders()
+    })
+    .pipe(
+      tap(res => console.log('Réponse désabonnement serveur :', res)),
+      catchError(err => {
+        console.error('Erreur HTTP désabonnement serveur :', err);
+        return throwError(() => err);
+      })
+    );
   }
+  // subscribe.service.ts
+getUserSubscriptions(): Observable<{id: number, theme_id: number, user_id: number}[]> {
+  return this.http.get<{subscribe: {id: number, theme_id: number, user_id: number}[]}>('/api/subscription', {
+    headers: this.getAuthHeaders()
+  }).pipe(
+    map(response => response.subscribe),
+    catchError(err => {
+      console.error('Erreur récupération abonnements :', err);
+      return of([]);
+    })
+  );
+}
+
+  
+  
+  
 }
